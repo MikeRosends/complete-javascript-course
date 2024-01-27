83d2d4c9a07ac13620f98bd08fccd6d6405dc7ea
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-01-27 00:26:55 +0000

    refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,55 +1,52 @@
 'use strict';
 
-/*
-document.querySelector('.message').textContent;
-document.querySelector('.message').textContent = 'Correct Number!';
-document.querySelector('.message').textContent;
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-*/
-
 let score = 20;
 let highscore = Number(document.querySelector('.highscore').textContent);
 
-document.querySelector('.check').addEventListener('click', checkNumber);
-document.querySelector('.again').addEventListener('click', goAgain);
 
 let secretNum = getRandomNum(1, 20);
 console.log(`Secret Number: ${secretNum}`);
+
+
+
+document.querySelector('.check').addEventListener('click', checkNumber);
 
 function checkNumber() {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+
   if (score <= 0) {
-    changeMessage('Game Over');
+    displayMessage('Game Over');
   } else if (!guess) {
-    changeMessage('Choose a number tho...');
+    displayMessage('Choose a number tho...');
   } else if (guess === secretNum) {
     graphicsWin();
-    changeMessage('Great Success!!');
+    displayMessage('Great Success!!');
     document.querySelector('.number').textContent = secretNum;
     if (score > highscore) {
       highscore = score;
       changeHighscore(highscore);
     }
-  } else if (guess > secretNum) {
-    changeMessage('Too high...');
-    score--;
-    changeScore(score);
-  } else if (guess < secretNum) {
-    changeMessage('Too low...');
+  } else if (guess !== secretNum) {
+    if (guess > secretNum) {
+      displayMessage('Too high...');
+    } else if (guess < secretNum) {
+      displayMessage('Too low...');
+    }
     score--;
     changeScore(score);
   }
 }
 
+
+document.querySelector('.again').addEventListener('click', goAgain);
+
 function goAgain() {
   graphicsGoAgain();
   secretNum = getRandomNum(1, 20);
   console.log(`New Secret Number: ${secretNum}`);
-  changeMessage('Start guessing...');
+  displayMessage('Start guessing...');
   score = 20;
   changeScore(score);
   document.querySelector('.number').textContent = '?';
@@ -61,7 +58,7 @@ function getRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + 1);
 }
 
-function changeMessage(message) {
+function displayMessage(message) {
   document.querySelector('.message').textContent = message;
 }
 
