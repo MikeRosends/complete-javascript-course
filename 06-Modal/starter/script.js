431df8cf6d481b2606c02cba6ba9ea338eca0107
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-01-27 01:42:40 +0000

    Model

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+overlay.addEventListener('click', closeModal);
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
