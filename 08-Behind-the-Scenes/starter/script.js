6a000dcd33f605a55b65866059c91890977a2b1e
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-02-03 21:50:47 +0000

    just a commit

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,16 +1,37 @@
 'use strict';
-console.log(this);
+//console.log(this);
 
 const calcAge = function (birthYear) {
-    console.log(2037 -birthYear);
-    console.log(this);
+    console.log(2024 -birthYear);
+    //console.log(this);
 }
 
 calcAge(1998);
 
 const calcAgeArrow = (birthYear) => {
-    console.log(2037 -birthYear);
-    console.log(this);
+    //console.log(2037 -birthYear);
+    //console.log(this);
 }
 
+console.log('-=-=-=-=-=-=-=-=-=-=-=');
+
 calcAgeArrow(1991);
+
+const mike = {
+    year: 1998,
+    calcAge: function() {
+        console.log(this);
+        console.log(2024 - this.year);
+    },
+};
+mike.calcAge();
+
+const bia = {
+    year: 2002,
+};
+
+bia.calcAge = mike.calcAge;
+bia.calcAge();
+
+const f = mike.calcAge;
+f();
