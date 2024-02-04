a10c743219b1f4c9b65f728b116c8680b862b159
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-02-04 04:54:02 +0000

    arrays and arguments

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -257,7 +257,6 @@ const[[, rating], [, ratingsCount]] = ratings;
 const ratingStars = [63405, 1808];
 const [fiveStarRatings, oneStarRatings, threeStarRatings=0] = ratingStars */
 
-
 /////////////////////////////////////////////////
 
 // Destructuring Objects
@@ -284,4 +283,60 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(`Oreder Recieved! ${this.starterMenu[starterIndex]} and 
+    ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '23:30',
+  address: 'Qta da Gaga Sul',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+    address: 'Qta da Gaga-Sul',
+    starterIndex: 1,
+  }
+);
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Assingning different values
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default Values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// MUtating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Objects and assingning different values
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
