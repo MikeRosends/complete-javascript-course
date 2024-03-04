8377c0aaa077ac97a563951ce9d56d1994236d05
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-03-04 22:45:44 +0000

    more classes

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -308,9 +308,52 @@ const restaurant = {
   },
 };
 
-console.log(restaurant.openingHours.mon?.open);
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `we are open ${properties.length} days a weak: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// for (const value of values) {
+//   console.log(value);
+// }
+
+// Entire Object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
 
 /* 
+console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`on day ${day}, we open at ${open}`);
+}
+
+// OPTIONAL CHAINING (?)
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'mike', email: 'mike.io' }];
+
+console.log(users[0]?.name || 'User does not exist');
+
 //// 1) DESTRUCTURING
 
 //SPREAD Operator - ... is on the right side of =
