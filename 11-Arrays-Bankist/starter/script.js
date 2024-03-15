8d530a60e9308c662539b993cb38110652f39ca4
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-03-15 17:21:24 +0000

    arrays beggining

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,29 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  // first empty the container
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__date">3 days ago</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+
+    // afterbegin defines the order in which the data appears
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +97,127 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-1)); // Last element of any array
+console.log(arr.slice(1, -2));
+console.log(arr.slice()); // Get a shallow copy of the array
+
+// SPLICE
+// console.log(arr.splice(2)); // Mutates the original array
+arr.splice(-1); // Remove the last element
+arr.splice(1, 2) // Delete 2 elements starting from position 1
+console.log(arr);
+
+// REVESRE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f']
+console.log(arr2.reverse()); // Mutates the original array
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// JOIN
+console.log(letters.join(' - '));
+
+const arr = [23, 11, 64];
+// same thing, for now
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting the last element
+console.log(arr[arr.length -1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('mike'.at(0));
+console.log('mike'.at(-1));
+
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movment ${i + 1}: You Deposited ${movement}`);
+  } else {
+    console.log(`Movment ${i + 1}: You Withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+
+// You cant break out of a forEach Loop
+// the parameters always go in this order - current element, index and the array itself
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movment ${index + 1}: You Deposited ${movement}`);
+  } else {
+    console.log(`Movment ${index + 1}: You Withdrew ${Math.abs(movement)}`);
+  }
+});
+
+
+// MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(currentValue, currentKey, mapItself) {
+  console.log(`${currentKey}: ${currentValue}`);
+})
+
+// SET
+const currenciesUnique = new Set(['USD', 'GBD', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+  // Here, value and key are the same so we use the '_'
+currenciesUnique.forEach(function(currentValue, _, mapItself) {
+  console.log(`${currentValue}: ${currentValue}`);
+})
+
+*/
+
+// CHALLENGE
+// test data1
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+// test data2
+const dogsJulia2 = [9, 16, 6, 8, 3];
+const dogsKate2 = [10, 5, 6, 1, 4];
+
+
+const checkDogs = function(dogsJulia, dogsKate) {
+  // Removing the cats
+ const justDogs = dogsJulia.slice(1, -2);
+ console.log(justDogs);
+
+ // Join both arrays
+ const allDogs = justDogs.concat(dogsKate);
+ console.log(allDogs);
+
+ // Log ages
+ allDogs.forEach(function(val, i) {
+  const age = val < 3 ? 'puppy' : 'adult';
+
+  if(val < 3) {
+    console.log(`Dog number ${i + 1} is still a ${age}`);
+  } else {
+    console.log(`Dog number ${i + 1} is an ${age}, and is ${val} years old`);
+  }
+ });
+};
+
+checkDogs(dogsJulia, dogsKate);
+console.log('--------');
+checkDogs(dogsJulia2, dogsKate2);
